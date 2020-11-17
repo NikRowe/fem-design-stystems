e0effdb87abcaecdbd6a1a2172fe5f944fc60441
Nik Rowe <nikrowedev@gmail.com>
2020-11-16 22:14:12 -0800

    Default Theme Primary Color & Font

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
-
-const primarySeafoam = "#59F0C7";
+import { defaultTheme, typeScale } from "../utils";
 
 const Button = styled.button`
-  background-color: ${primarySeafoam};
+  background-color: ${defaultTheme.primaryColor};
   padding: 12px 24px;
-  font-size: 1rem;
+  font-size: ${typeScale.paragraph};
   border-radius: 8px;
   min-width: 100px;
   cursor: pointer;
@@ -18,15 +17,15 @@ const PrimaryButton = styled(Button)`
 `;
 
 export const SeccondaryButton = styled(Button)`
-  border: 3px solid ${primarySeafoam};
-  color: ${primarySeafoam};
+  border: 3px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
   background: none;
 `;
 
 export const TertiaryButton = styled(Button)`
   border: none;
   background: none;
-  color: ${primarySeafoam};
+  color: ${defaultTheme.primaryColor};
 `;
 
 export default PrimaryButton;
