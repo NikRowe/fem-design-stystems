4814ad06bf7d2b57059c6c47c3518e1a705e0e71
Nik Rowe <nikrowedev@gmail.com>
2020-11-16 21:53:13 -0800

    DefaultTheme

---
+++ src/utils/themes.js
@@ -0,0 +1,12 @@
+import { seafoamGreen, neutral } from "./colors";
+import { primaryFont } from "./typography  ";
+
+export const defaultTheme = {
+  primaryColor: seafoamGreen[300],
+  primaryColorHover: seafoamGreen[400],
+  primaryColorActive: seafoamGreen[500],
+  textColorOnPrimary: neutral[100],
+  textColor: neutral[600],
+  textColorInverted: neutral[100],
+  primaryFont,
+};
