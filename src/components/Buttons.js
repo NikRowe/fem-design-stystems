90404e1cda5233588691c66940465bfb9708a15c
Nik Rowe <nikrowedev@gmail.com>
2020-11-17 21:12:28 -0800

    States

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,30 +2,62 @@ import styled from "styled-components";
 import { defaultTheme, typeScale, primaryFont, headerFont } from "../utils";
 
 const Button = styled.button`
-  background-color: ${defaultTheme.primaryColor};
-  padding: 12px 24px;
+  padding: 8px 12px;
   font-size: ${typeScale.paragraph};
   border-radius: 8px;
   min-width: 100px;
   cursor: pointer;
-  font-family: ${primaryFont};
+  font-family: ${defaultTheme.primaryFont};
+  transition: background-color 0.2s linear, color 0.2s linear;
+  &:hover {
+    background-color: ${defaultTheme.primaryColorHover};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+  &:focus {
+    outline: 3px solid ${defaultTheme.primaryColorHover};
+    outline-offset: 2px;
+  }
+  &:active {
+    background-color: ${defaultTheme.primaryColorActive};
+    border-color: ${defaultTheme.primaryColorActive};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
 `;
 
 const PrimaryButton = styled(Button)`
+  background-color: ${defaultTheme.primaryColor};
   border: none;
   color: white;
+
+  &:disabled {
+    background-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    cursor: not-allowed;
+  }
 `;
 
 export const SeccondaryButton = styled(Button)`
   border: 3px solid ${defaultTheme.primaryColor};
   color: ${defaultTheme.primaryColor};
   background: none;
+  &:disabled {
+    background: none;
+    border-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.disabled};
+    cursor: not-allowed;
+  }
 `;
 
 export const TertiaryButton = styled(Button)`
   border: none;
   background: none;
   color: ${defaultTheme.primaryColor};
+
+  &:disabled {
+    background: none;
+    color: ${defaultTheme.disabled};
+    cursor: not-allowed;
+  }
 `;
 
 export default PrimaryButton;
