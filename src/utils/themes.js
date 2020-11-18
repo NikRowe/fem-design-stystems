37b5d59960424358dedd9b250c4e0a63a9496d3b
Nik Rowe <nikrowedev@gmail.com>
2020-11-17 21:11:44 -0800

    Disabled Colors

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -2,11 +2,13 @@ import { seafoamGreen, neutral } from "./colors";
 import { primaryFont } from "./typography";
 
 export const defaultTheme = {
-  primaryColor: seafoamGreen[300],
+  primaryColor: seafoamGreen[500],
   primaryColorHover: seafoamGreen[400],
-  primaryColorActive: seafoamGreen[500],
+  primaryColorActive: seafoamGreen[300],
   textColorOnPrimary: neutral[100],
   textColor: neutral[600],
   textColorInverted: neutral[100],
+  disabled: neutral[400],
+  textOnDisabled: neutral[300],
   primaryFont,
 };
